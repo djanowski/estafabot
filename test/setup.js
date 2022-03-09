c1dc5bce604941bac6d20383cd9bf9ab4c681891
Damian Janowski <jano@dimaion.com>
2022-03-08 22:49:12 -0300

    Delete sample values.

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,3 @@
-//process.env.APP_KEY = 'a2PqdFZ23ooX9lEcaPb9xw';
-//process.env.APP_SECRET = 'xbLhdfbPXxbUDMXzXvvmxXmTMM5XFpjYXaaCo940';
-//process.env.ACCESS_TOKEN = '2492370458808282222-smmUrhdOYAmmMjvXsaatBjaAdAoAAv';
-//process.env.ACCESS_SECRET = 'amzODkCMY6tdtmaOIahML08Gv9bdlL41VP2a8nQlEYtbo';
-
 process.env.APP_KEY = 'E2PqdFZCloQX6lEcaPqdxw';
 process.env.APP_SECRET = 'xFL7dfqPyxbUDMlzXvvmxJmTMM5FFpjYSX7Coj48';
 process.env.ACCESS_TOKEN = '1492370358808289282-smdUrhdOYpQmMjvXsjatBjazdcoAMv';
