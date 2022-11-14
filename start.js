0d66a16d6501ac74abce0fae2c8a6cf3a89fc4ed
Damian Janowski <jano@dimaion.com>
2022-11-14 17:33:55 -0300

    Start immediately, also more reliability.

--- start.js
+++ start.js
@@ -6,7 +6,13 @@ const updateInterval = '1m';
 
 console.log('Checking every', updateInterval);
 
-setIntervalAsync(async () => {
-  await update();
-  fetch('https://cronitor.link/p/d0f88a8c67c94502beefda7035fc8c48/rWwpwY');
-}, ms(updateInterval));
+update()
+  .catch(console.error)
+  .then(() => {
+    setIntervalAsync(async () => {
+      await update().catch(console.error);
+      fetch(
+        'https://cronitor.link/p/d0f88a8c67c94502beefda7035fc8c48/rWwpwY'
+      ).catch(console.error);
+    }, ms(updateInterval));
+  });
