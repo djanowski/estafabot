03875adae7a7940b4dad5353592a1e04a93cd53a
Damian Janowski <jano@dimaion.com>
2022-12-11 22:50:40 -0300

    Clean.

--- saveArray.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-export default function saveArray(path, array) {
-  const lines = array.map(item => `${JSON.stringify(item)}`);
-  const stringified = `[\n${lines.join(',\n')}\n]`;
-  fs.writeFileSync(path, stringified);
-}
